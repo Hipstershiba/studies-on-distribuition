0776c28a52095dc1eab4d3504f020291310c48b7
hipstershiba <vitorrizzato@gmail.com>
2025-02-27 11:24:57 -0300

    Initialize data array with random values for graph plotting

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,8 +1,12 @@
-let data_array = [1, 2, 3, 4, 5];
+let data_array = [];
 let graph;
 
 function setup() {
   createCanvas(800, 450);
+  for (let i = 0; i < 100; i++) {
+    let value = random(0, 34);
+    data_array.push(value);
+  }
   graph = new Graph(data_array);
   background(220);
   graph.plot();

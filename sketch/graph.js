0268841ebad1764aecfada10fde89c2796057669
hipstershiba <vitorrizzato@gmail.com>
2025-02-27 09:24:55 -0300

    Enhance Graph class to include resolution calculation and improve axis labeling

--- sketch/graph.js
+++ sketch/graph.js
@@ -12,11 +12,13 @@ class Graph {
         this.data_length = this.data_array.length;
 
         let scaleFactor = 16;
-        this.width = (width/scaleFactor) * (scaleFactor - 2);
-        this.height = (height/scaleFactor) * (scaleFactor - 2);
-        let x = width/scaleFactor;
-        let y = this.height + height/scaleFactor;
+        this.width = (width / scaleFactor) * (scaleFactor - 2);
+        this.height = (height / scaleFactor) * (scaleFactor - 2);
+        let x = width / scaleFactor;
+        let y = this.height + height / scaleFactor;
         this.origin = createVector(x, y);
+
+        this.resolution = this.width / (this.data_length - 1);
     }
 
     plot() {
@@ -28,10 +30,21 @@ class Graph {
 
     draw_graph() {
         stroke(0);
-        strokeWeight(1);
+        strokeWeight(2);
         noFill();
         // X axis
         line(this.origin.x, this.origin.y, this.origin.x + this.width, this.origin.y);
+        for (let i = 0; i < this.data_length; i++) {
+            let x = this.origin.x + (i * this.resolution);
+            push();
+            stroke(0);
+            strokeWeight(1);
+            line(x, this.origin.y, x, this.origin.y - 5);
+            noStroke();
+            fill(0);
+            text(i, x, this.origin.y + 15);
+            pop();
+        }
         // Y axis
         line(this.origin.x, this.origin.y, this.origin.x, this.origin.y - this.height);
     }
